7e299cd97c7c2ccaef2b5b1482ec12693cba700c
Oliver Almalel <oliver.almalel@gmail.com>
2019-07-18 12:50:25 -0700

    build remove and add mutations

---
+++ src/mutations/RemoveSelectedWidgetIdMutation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { WidgetTable } from '../components'
+
+export const REMOVE_SELECTED_WIDGET_ID_MUTATION = gql `
+  mutation RemoveSelectedWidgetId($widget: ID) {
+    removeSelectedWidgetId(widgetId: $widgetId) @client
+  }
+`
+
+export const RemoveSelectedWidgetId = props =>
+  <Mutation mutation={ REMOVE_SELECTED_WIDGET_ID_MUTATION }>
+    {mutateRemoveSelectedWidgetId =>
+      <WidgetTable {...props}
+        onRemoveSelectedWidgetId={ widgetId =>
+          mutateRemoveSelectedWidgetId({ variables: widgetId })
+        }
+      />
+    }
+  </Mutation>
